a073f363ec632a06b5a80c843732f63310cae328
selina <q339li@uwaterloo.ca>
2023-07-04 10:32:45 -0400

    Initial Commit

---
+++ src/main/java/ca/uwaterloo/drinkmasterapi/DrinkmasterApiApplication.java
@@ -0,0 +1,13 @@
+package ca.uwaterloo.drinkmasterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkmasterApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrinkmasterApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/uwaterloo/drinkmasterapi/DrinkmasterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.uwaterloo.drinkmasterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkmasterApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
